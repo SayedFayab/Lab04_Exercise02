e7601e5659db27fd6fccd3a8b64667a1c65db453
SayedFayab <sayedfayab@gmail.com>
2024-04-01 12:44:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/group5sayed_and_selenecomp304sec005_lab04_ex02/Program.kt
@@ -0,0 +1,5 @@
+package com.example.group5sayed_and_selenecomp304sec005_lab04_ex02
+
+data class Program(val name:String,val description:String){
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Group5(Sayed_and_Selene)COMP304SEC005_Lab04_Ex02"
+include(":app")
